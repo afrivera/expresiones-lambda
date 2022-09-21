e9b9c2fb3262af4debb1540de6404691f2219480
afrivera <andresrfelip@hotmail.com>
2022-09-20 20:23:57 -0500

    ejemplo con interfaces funcionales java

---
+++ src/org/afrivera/javaa8/lambda/aritmetica/Aritmetica.java
@@ -0,0 +1,6 @@
+package org.afrivera.javaa8.lambda.aritmetica;
+
+@FunctionalInterface // para expresar que es una función lambda
+public interface Aritmetica {
+    double operacion(double a, double b);
+}
